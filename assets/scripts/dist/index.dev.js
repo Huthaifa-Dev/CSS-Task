f5164dd70d47e7cc3d0c49bb223bd03c45c8fe25
Huthaifa <huthaifajamal2@gmail.com>
2022-08-03 18:06:45 +0300

    added projects section and articles

--- assets/scripts/dist/index.dev.js
+++ assets/scripts/dist/index.dev.js
@@ -2,21 +2,37 @@
 
 var sections = document.querySelectorAll("section");
 var navLi = document.querySelectorAll("nav ul li");
+var main = document.querySelector('.main');
 window.addEventListener("scroll", function () {
   var current = "";
   sections.forEach(function (section) {
     var sectionTop = section.offsetTop;
     var sectionHeight = section.clientHeight;
+    console.log(sectionTop, sectionHeight); // console.log(window.innerHeight, window.innerWidth);
+
+    console.log(sectionTop - sectionHeight / 3, pageXOffset);
 
     if (pageYOffset >= sectionTop - sectionHeight / 3) {
       current = section.getAttribute("id");
     }
   });
   navLi.forEach(function (li) {
-    li.classList.remove("active");
+    li.classList.remove("active"); // console.log(li.classList.contains(`${current}_link`));
+    // console.log(li.classList[1])
+    // console.log(current);
 
     if (li.classList.contains("".concat(current, "_link"))) {
       li.classList.add("active");
     }
   });
 });
+
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+
+  if (element.classList.contains("experience")) {
+    console.log(rect.top, rect.left, rect.bottom, window.innerHeight, rect.right);
+  }
+
+  return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,10 +1,14 @@
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll("nav ul li");
+const main = document.querySelector('.main');
 window.addEventListener("scroll", () => {
     let current = "";
     sections.forEach((section) => {
         const sectionTop = section.offsetTop;
         const sectionHeight = section.clientHeight;
+        console.log(sectionTop, sectionHeight);
+        // console.log(window.innerHeight, window.innerWidth);
+        console.log(sectionTop - sectionHeight / 3, pageXOffset);
         if (pageYOffset >= sectionTop - sectionHeight / 3) {
             current = section.getAttribute("id");
         }
@@ -12,8 +16,27 @@ window.addEventListener("scroll", () => {
 
     navLi.forEach((li) => {
         li.classList.remove("active");
+        // console.log(li.classList.contains(`${current}_link`));
+        // console.log(li.classList[1])
+        // console.log(current);
         if (li.classList.contains(`${current}_link`)) {
             li.classList.add("active");
         }
     });
 });
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    if (element.classList.contains("experience")) {
+        console.log(rect.top,
+            rect.left,
+            rect.bottom, window.innerHeight,
+            rect.right
+        );
+    }
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
